b9aabdb551e5241f4eaca0fe322d248b36327330
AsHuzaifa <mohammedhuzaifa464@gmail.com>
2024-12-11 18:35:51 +0530

    Loop completed

--- LeetCode/Weekly Contest 427/1.py
+++ LeetCode/Weekly Contest 427/1.py
@@ -5,8 +5,20 @@ class Solution:
         Read the problem statement and let me know more about what is needed.
         """
         # Write code to transform array here:
+        n= len(nums)
+        result = [0] * n
+        for i in range(n):
+            if nums[i]==0:
+                result[i]=nums[i]
+            elif nums[i]>0:
+                result[i]=nums(i+nums[i])
+            else:
+                result[i]=nums(i+nums[i])
+        return result
+
         return [] # Returning an empty list for now.
 
 answer = Solution()
 transformed_array = answer.constructTransformedArray(nums = [3,-2,1,1])
 print(f"Transformed Array: {transformed_array}")
+answer.constructTransformedArray()
