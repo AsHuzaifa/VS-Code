6f807524de17ebb910d2c0a1d9065a4543babf78
AsRumi <mohammedmutahar3@gmail.com>
2024-12-11 17:56:12 +0530

    Modified 1.py

--- LeetCode/Weekly Contest 427/1.py
+++ LeetCode/Weekly Contest 427/1.py
@@ -1,3 +1,12 @@
 class Solution:
     def constructTransformedArray(self, nums: list[int]) -> list[int]:
-        
+        """
+        Function takes a list as an input (array) and returns another list as an ouput (array).
+        Read the problem statement and let me know more about what is needed.
+        """
+        # Write code to transform array here:
+        return [] # Returning an empty list for now.
+
+answer = Solution()
+transformed_array = answer.constructTransformedArray(nums = [3,-2,1,1])
+print(f"Transformed Array: {transformed_array}")
