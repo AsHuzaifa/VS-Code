d11130bf49594fd5c3fd71198daca42a62d316b4
AsRumi <mohammedmutahar3@gmail.com>
2024-12-11 19:26:22 +0530

    Solution

--- LeetCode/Weekly Contest 427/1.py
+++ LeetCode/Weekly Contest 427/1.py
@@ -5,24 +5,24 @@ class Solution:
         Read the problem statement and let me know more about what is needed.
         """
         # Write code to transform array here:
-        result = [] # Can directly append
+        result = []
         copy = nums * 3
         for i in range(len(nums)):
             if nums[i] == 0:
                 result.append(nums[i])
             elif nums[i] > 0:
                 position = nums[i]//len(nums)
-                position = nums[i] - position
+                position = nums[i] - (position*len(nums))
                 position = len(nums) + i + position
                 result.append(copy[position])
             else:
                 x = abs(nums[i])
                 position = x//len(nums)
-                position = x - position
+                position = x - (position*len(nums))
                 position = len(nums) + i - position
                 result.append(copy[position])
         return result
 
 answer = Solution()
-transformed_array = answer.constructTransformedArray(nums = [[3, -2, 1, 1]])
+transformed_array = answer.constructTransformedArray(nums = [3, -2, 7, 10])
 print(f"Transformed Array: {transformed_array}")
